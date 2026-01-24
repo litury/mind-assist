8eea45f1c9dfa87522609d063b3732d48edcf8a2
Litvinov Y. <litury@icloud.com>
2026-01-25 00:25:28 +0500

    refactor(web): убрал StatsBar и TechStack

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,6 @@
 import { Hero } from '@/components/sections/Hero'
-import { StatsBar } from '@/components/sections/StatsBar'
 import { UseCases } from '@/components/sections/UseCases'
 import { ModulesGrid } from '@/components/sections/ModulesGrid'
-import { TechStack } from '@/components/sections/TechStack'
 import { Security } from '@/components/sections/Security'
 import { Roadmap } from '@/components/sections/Roadmap'
 import { Waitlist } from '@/components/sections/Waitlist'
@@ -12,10 +10,8 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
-      <StatsBar />
       <UseCases />
       <ModulesGrid />
-      <TechStack />
       <Security />
       <Roadmap />
       <Waitlist />
